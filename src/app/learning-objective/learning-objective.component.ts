5193c4859c20b462e18ba39b7fb9d792a4c3e43a
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2022-04-13 15:36:47 +0200

    based off data

--- src/app/learning-objective/learning-objective.component.ts
+++ src/app/learning-objective/learning-objective.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-learning-objective',
@@ -11,5 +11,5 @@ export class LearningObjectiveComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  @Input() data: any;
 }
