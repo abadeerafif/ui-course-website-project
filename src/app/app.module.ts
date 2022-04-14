2c20a33f17d81b0144dcc9ca0e3fff565b5e44e8
abadeerafif <abadeer@hotmail.com>
2022-04-14 14:48:49 +0200

    na2es seemore we tarbeet bel dummy database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RegisterComponent } from './register/register.component';
 import { CourseContentComponent } from './course-content/course-content.component';
 import { LearningObjectiveComponent } from './learning-objective/learning-objective.component';
 import { InstructorComponent } from './instructor/instructor.component';
+import { SyllabusComponent } from './syllabus/syllabus.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { InstructorComponent } from './instructor/instructor.component';
     CourseContentComponent,
     LearningObjectiveComponent,
     InstructorComponent,
+    SyllabusComponent,
   ],
   imports: [
     BrowserModule,
