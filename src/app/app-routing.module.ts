2fc9c10a75a4689e55c78f2b73b278a00b78d5cb
PeterMagdyIskander <PeterMagdyIskander@gmail.com>
2022-04-30 23:12:48 +0200

    [ADD] 3 new routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import {CourseContentComponent} from './course-content/course-content.component';
+import { CourseContentComponent } from './course-content/course-content.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { ExploreComponent } from './explore/explore.component';
 import { VideoComponent } from './video/video.component';
 import { AddCourseComponent } from './add-course/add-course.component';
-
+import { AcceptStudentRegistrationComponent } from './accept-student-registration/accept-student-registration.component';
+import { AllUsersDetailsComponent } from './all-users-details/all-users-details.component';
 const routes: Routes = [
-  {path:'mainMenu',component:MainMenuComponent},
-  {path:'login',component:LoginComponent},
-  {path:'register',component:RegisterComponent},
-  {path:'explore',component:ExploreComponent},
-  {path:'addCourse',component:AddCourseComponent},
-  {path:'course-content/:id',component:CourseContentComponent},
-  {path:'videos/:id',component:VideoComponent},
-  {path:'',redirectTo:'/mainMenu',pathMatch:'full'},
+  { path: 'mainMenu', component: MainMenuComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'explore', component: ExploreComponent },
+  { path: 'addCourse', component: AddCourseComponent },
+  {
+    path: 'accept-student-registration',
+    component: AcceptStudentRegistrationComponent,
+  },
+  {
+    path: 'all-user-details',
+    component: AllUsersDetailsComponent,
+  },
+  { path: 'addCourse', component: AddCourseComponent },
+  { path: 'course-content/:id', component: CourseContentComponent },
+  { path: 'videos/:id', component: VideoComponent },
+  { path: '', redirectTo: '/mainMenu', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
