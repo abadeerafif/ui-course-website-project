b25ba1d68010c3753e7317c0206698fd1bc4f02b
PeterGeorgee <petergeoorge@gmail.com>
2022-05-15 22:19:25 +0200

    Calendar integration

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseDataService } from '../course-data.service';
 
 @Component({
   selector: 'app-calendar',
@@ -8,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 export class CalendarComponent implements OnInit {
 
   public courseList: string[] = [];
-  constructor() { }
+  public allAvailableCourses: any[] = [];
+  public filteredCourses: any[] = [];
+  constructor(public courseData: CourseDataService) { }
 
-  ngOnInit(): void {
+
+
+  async ngOnInit(): Promise<void> {
+    this.allAvailableCourses = await this.courseData.getCourses();
+    this.courseList=await this.courseData.AuthedUser.courses;
+
+    for(var course of this.courseList){
+      for(var allcourse of this.allAvailableCourses){
+        if(course==allcourse.courseName){
+          this.filteredCourses.push(allcourse);
+          break;
+        }
+      }
+    }
+
+    for(var mycourse of this.filteredCourses){
+      var date = mycourse.start.toDate().toDateString();
+      mycourse.start=date;
+      var enddate = mycourse.end.toDate().toDateString();
+      mycourse.end=enddate;
+    }
   }
 
 }
